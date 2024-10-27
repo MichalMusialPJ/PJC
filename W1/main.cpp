9edf77df647f0ac522f6b4cac3278cb4904f3fcd
MichalMusialPJ <93324928+MichalMusialPJ@users.noreply.github.com>
2024-10-27 19:04:09 +0100

    Adding solution for first exercise

--- W1/main.cpp
+++ W1/main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World! W1" << std::endl;
+auto boxPrint(std::vector<std::string> vec, char chr = '*') -> int {
+    auto longestLength = 0;
+    auto myChar = chr;
+
+// Finding the longest word in vector
+    for(std::string const& element : vec ){
+        if(element.size() > longestLength){
+            longestLength = element.size();
+        }
+    }
+
+// If word shorter than longest add blank char until it has the same length
+    for(std::string & element : vec){
+        auto numberOfBlanks = longestLength - element.size();
+        for( auto i = 0; i < numberOfBlanks; i++){
+            element += " ";
+        }
+    }
+
+// Printing box
+    for(int i=0; i <= vec.size(); i++){
+        if(i==0 || i == vec.size()){
+            for(int j=0; j < longestLength + 2; j++){
+                std::cout << myChar;
+            }
+            std::cout << "\n";
+        }
+        if(i != vec.size()) {
+            std::cout << myChar << vec[i] << myChar << "\n";
+        }
+    }
+    std::cout << "\n";
+
     return 0;
+}
+
+auto main() -> int {
+
+//    Box print
+    boxPrint({"a", "quick", "brown", "fox"});
+    boxPrint({"a", "quick", "brown", "fox"}, '*');
+    boxPrint({"a", "quick", "brown", "fox"}, '#');
+    boxPrint(std::vector<std::string>{"a", "quick", "brown", "fox"}, '#');
+    boxPrint({""});
+    boxPrint({});
+    boxPrint({}, '#');
+    boxPrint({"", "hmmm", ""});
+    boxPrint({"", "", ""});
 }
---
+++ W1/notes.md
@@ -0,0 +1,3 @@
+# Lecture 1
+
+Already done before, maybe i ll come back here before exam :x
